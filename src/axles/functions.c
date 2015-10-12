2543548e7011ab1f5cbd26c49ca359b18b2174f8
Wojtek Gumuła <wgumula@student.agh.edu.pl>
2015-10-12 21:27:13 +0200

    Zmiana struktury kodu, poprawa działania systemu budowania.

--- src/axles/functions.c
+++ src/axles/functions.c
@@ -8,7 +8,7 @@
 #include <string.h>
 
 #include "functions.h"
-#include "version.h"
+#include "../include/version.h"
 
 char output_filename[80];
 
--- src/watcher/watcher.c
+++ src/watcher/watcher.c
@@ -21,7 +21,7 @@
 #include <getopt.h>
 #include <stdlib.h>
 
-#include "version.h"
+#include "../include/version.h"
 
 #define EVENT_SIZE  ( sizeof (struct inotify_event) )
 #define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
