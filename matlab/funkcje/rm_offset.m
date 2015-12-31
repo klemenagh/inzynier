8955dbb9f54dc4967175713df6714b3b800295bb
Wojtek Gumuła <wgumula@student.agh.edu.pl>
2015-12-31 15:21:13 +0100

    Usunięcie plików niewykorzystywanych w projekcie.

--- matlab/funkcje/rm_offset.m
+++
@@ -1,7 +0,0 @@
-function s=rm_offset(s, n)
-% Funkcja usuwa offset na podstawie �redniej z n pocz�tkowych warto�ci
-offset=sum(s(1:n))/n;
-fprintf('\toffset = %f\n', offset);
-% fprintf('%f ', offset);
-s=s-offset;
-end
