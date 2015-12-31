471a191cd31532621dca154e4cbe7cd3a71a10c3
Wojtek Gumuła <wgumula@student.agh.edu.pl>
2015-12-31 15:16:14 +0100

    Umożliwienie uruchomienia aplikacji ootb.

--- examples/test.sh
+++ examples/test.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
-cd test
-./test_alg.py
-cd ..
+# skrypt wraz z configuracją zawartą w katalogu test
+# pozwala domyślnie na wywołanie testów aplikacji
+# na podstawie danych ze stanowiska nr 1
+# Drzewo katalogów testowych względem głównego
+# katalogu projektu ma postać
+# data
+# ├── baza1
+# │   ├── ciezarowy_2os
+# │   ├── ciezarowy_3os
+# │   ├── ciezarowy_4os
+# │   ├── ciezarowy_5os
+# │   ├── ciezarowy_5os_up
+# │   ├── dostawczy
+# │   ├── osobowy_alus
+# │   └── osobowy_stal
+# └── baza2
+# i wykorzystywane są pliki z podkatalogu baza1
+# konfigurację można zmienić poprzez plik config.json
+if [[ -d data/baza1 ]]; then
+    cd test
+    ./test_alg.py
+    cd ..
+else
+    echo "Katalog data/baza1 nie istnieje. Jeśli zmodyfikowano konfigurację, należy bezpośrednio uruchomić skrypt test_alg.py z poziomu katalogu test."
+fi
+
--- run.sh
+++ run.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 
 ## stanowisko 1
-#. ./examples/example1.sh
+echo "-------------------------------------------------"
+echo "Przykład przetworzenia danych ze stanowiska nr 1."
+. ./examples/example1.sh
 
 ## stanowisko 2
-#. ./examples/example2.sh
-
-## wykorzystanie gui i modułu watcher do obserwowania ścieżki
-#. ./examples/example_watcher_gui.sh
+echo "-------------------------------------------------"
+echo "Przykład przetworzenia danych ze stanowiska nr 2."
+. ./examples/example2.sh
 
 ## testy
 # plik wykonujący testy znajduje się w katalogu test
-# wykorzystuje dane z archiwum `algorytm2_in_matlab` zakładając, że jest ono rozpakowane w głównym katalogu projektu
+# wykorzystuje dane z archiwum `algorytm2_in_matlab` zakładając, że jest ono rozpakowane w katalogu data/baza1
 # można to zmodyfikować poprzez plik config.json (`root_directory`)
-#. ./examples/test.sh
+echo "-------------------------------------------------"
+echo "Przykład wywołania testów"
+. ./examples/test.sh
+
+
+## wykorzystanie gui i modułu watcher do obserwowania ścieżki
+# . ./examples/example_watcher_gui.sh
 
 ## strony podręczników
-#man axles
-#man watcher
+# man axles
+# man watcher
